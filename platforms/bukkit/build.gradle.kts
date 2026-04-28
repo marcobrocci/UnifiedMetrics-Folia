11d393c3b5f6448cf62fce638e55e5251486c25a
Marco Brocci <marco.brocci@gmail.com>
2026-04-28 10:42:11 +0200

    fix(bukkit): update Paper API to 1.21.1-R0.1-SNAPSHOT

--- platforms/bukkit/build.gradle.kts
+++ platforms/bukkit/build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 
 dependencies {
     api(project(":unifiedmetrics-core"))
-    compileOnly("io.papermc.paper", "paper-api", "1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper", "paper-api", "1.21.1-R0.1-SNAPSHOT")
 }
 
 tasks {
