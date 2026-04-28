4f559f5429ac3adeaa579b14d7f8b79d8d52c181
Marco Brocci <marco.brocci@gmail.com>
2026-04-28 18:51:31 +0200

    fix(build): re-enable Fabric, upgrade loom 1.10.5 -> 1.12.1 for Gradle 8.13 compat

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ include(modulePrefix + platformPrefix + "bukkit")
 //include(modulePrefix + platformPrefix + "minestom")
 include(modulePrefix + platformPrefix + "velocity")
 include(modulePrefix + platformPrefix + "bungee")
-//include(modulePrefix + platformPrefix + "fabric")
+include(modulePrefix + platformPrefix + "fabric")
 
 include(modulePrefix + driverPrefix + "influx")
 include(modulePrefix + driverPrefix + "prometheus")
@@ -43,7 +43,7 @@ project(modulePrefix + platformPrefix + "bukkit").projectDir = File(platformsDir
 //project(modulePrefix + platformPrefix + "minestom").projectDir = File(platformsDir, "minestom")
 project(modulePrefix + platformPrefix + "velocity").projectDir = File(platformsDir, "velocity")
 project(modulePrefix + platformPrefix + "bungee").projectDir = File(platformsDir, "bungee")
-//project(modulePrefix + platformPrefix + "fabric").projectDir = File(platformsDir, "fabric")
+project(modulePrefix + platformPrefix + "fabric").projectDir = File(platformsDir, "fabric")
 
 val driversDir = File(rootDir, "drivers")
 project(modulePrefix + driverPrefix + "influx").projectDir = File(driversDir, "influx")
